6925c207441a5b10edfdc7d66302790f452aedb2
Jordan Weiss <weissjs@vcu.edu>
2019-10-20 12:54:16 -0400

    sign extension changed to 6bit

--- sign_extend.vhd
+++ sign_extend.vhd
@@ -3,14 +3,14 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity sign_extend is
-	port( len_8 : in std_logic_vector(7 downto 0);
+	port( len_6 : in std_logic_vector(5 downto 0);
 		  len_16 : out std_logic_vector(15 downto 0));
 end sign_extend;
 
 architecture behavioral of sign_extend is
 begin
 	
-    len_16 <= std_logic_vector(resize(signed(len_8), len_16'length));
+    len_16 <= std_logic_vector(resize(signed(len_6), len_16'length));
 	
 end behavioral;
 			
