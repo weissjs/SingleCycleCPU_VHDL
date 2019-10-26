a3db417ee108663d7afd2e07966e1b0a95c61fe8
Jordan Weiss <weissjs@vcu.edu>
2019-10-26 13:54:03 -0400

    top level changes, mux added

--- data_memory.vhd
+++ data_memory.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity data_memory is
-	port(   address :  in std_logic_vector(15 downto 0);
+	port(   address :  in std_logic_vector(7 downto 0);
 		    Write_data  : in std_logic_vector(15 downto 0);
 			Read_data : out std_logic_vector(15 downto 0);
 			MemWrite : in std_logic;
--- lab4_toplevel.vhd
+++ lab4_toplevel.vhd
@@ -24,6 +24,8 @@ architecture behavioral of l4_top is
 	signal instruction_sig   : std_logic_vector(15 downto 0);
 	signal program_count_sig : std_logic_vector(15 downto 0);
 	signal write_data_sig : std_logic_vector(15 downto 0);
+	signal write_addr_sig : std_logic_vector(3 downto 0);
+	signal read_data_mem_sig : std_logic;
 	signal result_alu_sig : std_logic_vector(15 downto 0);
 	signal RegDst_sig     : std_logic;
 	signal Jump_sig       : std_logic;
@@ -34,28 +36,30 @@ architecture behavioral of l4_top is
 	signal MemWrite_sig   : std_logic;
 	signal ALUSrc_sig     : std_logic;
 	signal RegWrite_sig   : std_logic;
+	signal PC_sig         : std_logic(6 downto 0);
 	
 
 begin
 
 
-		DUT : entity work.instruction_mem(behavioral)
-      port map( Rd => instruction_sig(3 downto 0),
-				Rs => instruction_sig(11 downto 8),
-				Rt => instruction_sig(7 downto 4),
-				busW => write_data_sig,
-				RegWr => RegWrite_sig,
-				clk => Clk,
-				busA => read_data_1_sig,
-				busB => read_data_2_sig);
+		Instruciton memory : entity work.instruction_mem(behavioral)
+      port map( Read_address => PC_sig,
+				Instruction => instruction_sig,
+				clk => Clk;
+	
+		mux1 : entity work.mux_4(behavioral)
+	  port map( in_1 => instruction_sig(7 downto 4),
+				in_2 => instruction_sig(3 downto 0),
+				sel => RegDst_sig,
+				out_mux => write_addr_sig);
+				
+				
+		sign extend : entity work.sign_extend(behavioral)
+	  port map( len_4 => instruction_sig(3 downto 0),
+				len_16 => sign_extended_sig(15 downto 0));
 
-
-
-
-
-
-		DUT : entity work.reg_file(behavioral)
-      port map( Rd => instruction_sig(3 downto 0),
+		reg : entity work.reg_file(behavioral)
+      port map( Rd => write_addr_sig,
 				Rs => instruction_sig(11 downto 8),
 				Rt => instruction_sig(7 downto 4),
 				busW => write_data_sig,
@@ -64,8 +68,15 @@ begin
 				busA => read_data_1_sig,
 				busB => read_data_2_sig);
 				
-		DUT0 : entity work.alu(behavioral)
-      port map( busA => read_data_2_sig,
+		mux2 : entity work.mux_16(behavioral)
+	  port map( in_1 => read_data_2_sig,
+				in_2 => sign_extended_sig,
+				sel => ALUSrc_sig,
+				out_mux => input_alu_b);
+				
+				
+		alu : entity work.alu(behavioral)
+      port map( busA => read_data_1_sig,
 				busB => input_alu_b,
 				mode => ALUctr,     --learn whats happening here. leave for now
 				Overflow => Overflow,
@@ -74,18 +85,33 @@ begin
 				Zero => Zero);
 	
 	
-		DUT : entity work.data_memory(behavioral)
-      port map( Rd => instruction_sig(3 downto 0),
-				Rs => instruction_sig(11 downto 8),
-				Rt => instruction_sig(7 downto 4),
-				busW => write_data_sig,
-				RegWr => RegWrite_sig,
-				clk => Clk,
-				busA => read_data_1_sig,
-				busB => read_data_2_sig);
-	
-
-	
+		data memory : entity work.data_memory(behavioral)
+      port map( address => result_sig(7 downto 0),
+				Write_data => read_data_2_sig,
+				Read_data => read_data_mem_sig,
+				MemWrite => MemWrite_sig,
+				MemRead => MemRead_sig,
+				clk => Clk);
+				
+		mux3 : entity work.mux_16(behavioral)
+	  port map( in_1 => read_data_mem_sig,
+				in_2 => result_sig,
+				sel => MemtoReg_sig,
+				out_mux => write_data_sig);
+				
+		control : entity work.control_unit(behavioral)
+	  port map( instr_op => instruction(15 downto 12), 
+				RegDst => RegDst_sig,
+				Jump => Jump_sig,
+				Branch => Branch_sig,
+				MemRead => MemRead_sig,
+				MemtoReg => MemtoReg_sig,
+				ALUSrc => ALUSrc_sig,
+				MemWrite => MemWrite_sig,
+				ALUOP => ALUOp_sig,
+				RegWrite => RegWrite_sig);
+				
+		
 	
 end behavioral;
 			
